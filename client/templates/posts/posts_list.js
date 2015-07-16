057a7d7ffa2bd723e485b314f268c1885e17667f
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-16 02:57:36 -0500

    added 'edit post' feature with allow/deny in server-side

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -3,11 +3,3 @@ Template.postsList.helpers({
 		return Posts.find({}, {sort: {submitted: -1}});
 	}
 });
-
-Template.postItem.helpers({
-	domain: function () {
-		var a = document.createElement('a');
-		a.href=this.url;
-		return a.hostname;
-	}
-});
