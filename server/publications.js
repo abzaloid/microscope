b846f7a69488c423d09eec05ca577933c31fed7b
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-16 14:42:46 -0500

    comments now are more distributed in MongoDB to use CPU more intelligent without loading all comments in login

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,11 @@ Meteor.publish('posts', function() {
 	return Posts.find();
 });
 
-Meteor.publish('comments', function () {
-	return Comments.find();
+Meteor.publish('comments', function (postId) {
+	check(postId, String);
+	return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function () {
+	return Notifications.find({userId: this.userId, read: false});
 });
