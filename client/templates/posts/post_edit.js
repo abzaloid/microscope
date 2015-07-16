7cc3934f6730d4ffbb0fb98bdc8dd81cca5cbddb
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-16 03:06:51 -0500

    now alerts are changed by throwError function

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -11,7 +11,7 @@ Template.postEdit.events({
 
 		Posts.update(currentPostId, {$set: postProperties}, function (error) {
 			if (error) {
-				alert(error.reason);
+				throwError(error.reason);
 			} else {
 				Router.go('postPage', {_id: currentPostId});
 			}
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,10 +9,10 @@ Template.postSubmit.events({
 
 		Meteor.call('postInsert', post, function(error, result) {
 			if (error)
-				return alert(error.reason);
+				return throwError(error.reason);
 
 			if (result.postExists) 
-				alert('This link has already been posted');
+				throwError('This link has already been posted');
 
 			Router.go('postPage', {_id: result._id});
 		});
