2d716aed3b8a016bf98309b68b35c1ee57590744
Abzal Serekov <abzal.serekov@gmail.com>
2015-07-17 00:49:23 -0500

    added animations with _uihook

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,31 @@
+Template.postsList.onRendered(function () {
+	this.find('.wrapper')._uihooks = {
+		moveElement: function (node, next) {
+			var $node = $(node), $next = $(next);
+			var oldTop = $node.offset().top;
+			var height = $node.outerHeight(true);
+
+			var $inBetween =$next.nextUntil(node);
+			if ($inBetween.length === 0)
+				$inBetween = $node.nextUntil(next);
+
+			$node.insertBefore(next);
+
+			var newTop = $node.offset().top;
+
+			$node
+				.removeClass('animate')
+				.css('top', oldTop - newTop);
+
+			$inBetween
+				.removeClass('animate')
+				.css('top', oldTop < newTop ? height : -1 * height);
+
+			$node.offset();
+
+			$node.addClass('animate').css('top', 0);
+			$inBetween.addClass('animate').css('top', 0);
+
+		}
+	}
+});
